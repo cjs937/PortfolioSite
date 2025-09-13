b9e342cd49655a8fedd4a3af9eaa79d211e0cb14
cjs937 <smithcory937@gmail.com>
2025-09-13 18:59:48 -0400

    Added a model and layout for the modal portfolio windows

---
+++ Source/PortfolioSource/Models/PortfolioModalViewModel.cs
@@ -0,0 +1,12 @@
+namespace PortfolioSource.Models
+{
+    public class PortfolioModalViewModel
+    {
+        public string ModalID { get; set; } = "";
+        public string Title { get; set; } = "EmptyTitle";
+        public string SizeClass { get; set; } = "modal-xl";
+        public string ImageURL { get; set; } = "";
+        public string BodyHtml { get; set; } = "EmptyBody";
+        //public string FooterHtml { get; set; }
+    }
+}
