9fd11ad0e34146ef35c1765572f4430c93d44ecb
cjs937 <31425502+cjs937@users.noreply.github.com>
2025-10-14 14:36:34 -0400

    CSS in modal layout. Added info for other portfolio items

--- Source/PortfolioSource/Models/PortfolioItemViewModel.cs
+++ Source/PortfolioSource/Models/PortfolioItemViewModel.cs
@@ -7,10 +7,11 @@ namespace PortfolioSource.Models
         public string Icon { get; set; } = "fa-star";
         public string Title { get; set; } = "EmptyTitle";
         public string ImageURL { get; set; } = "";
+        public string WebsiteURL { get; set; } = "";
         public Dictionary<string, string> Badges { get; set; } = new Dictionary<string, string>(); //Title, Color
         public string BodyIntro { get; set; } = "";
         public string BodyMain { get; set; } = "";
-        public List<string> Contributions { get; set; } = new List<string>();
+        public Dictionary<string, string> Contributions { get; set; } = new Dictionary<string, string>(); //Contribution, Description HTML
         public string Footer { get; set; } = "";
         public bool UseJsonURLS { get; set; } = false;
         public List<string> GalleryURLs { get; set; } = new List<string>();
